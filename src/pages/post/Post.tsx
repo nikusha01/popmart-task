b8706fb3795e8abe558e8aac0539a29fdc61832c
nikusha01 <nikamaghradze01@gmail.com>
2024-04-05 18:23:16 +0400

    add post

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 
-import './Post.css'
+import "./Post.css";
+import { IPost } from "../../interfaces/posts";
 
 export default function Post() {
+  const { id } = useParams<string>();
+  const url = "https://jsonplaceholder.typicode.com/posts?id=" + id;
+  const { data: post, isPending, error } = useFetch<IPost>(url);
+
+  console.log(post);
   return (
-    <div>Post</div>
-  )
+    <div className="post">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading ...</p>}
+      {post && (
+        <>
+          <h2 className="page-title">{post.title}</h2>
+          <p className="post-body">{post.body}</p>
+          <Link to="/">Back to All Posts</Link>
+        </>
+      )}
+    </div>
+  );
 }

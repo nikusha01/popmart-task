e815710123237e466bfba1e6bcb1d79e88c24eb9
nikushaa <n.magradze@lemondo.biz>
2024-04-08 12:04:31 +0400

    add infinite scroll

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,51 @@
-import PostList from "../../components/PostList";
+import { useState, useEffect } from 'react';
 import { useFetch } from "../../hooks/useFetch";
-
-import "./Home.css";
-import { IPost } from '../../interfaces/posts'
 import { useSearch } from "../../context/Appcontext";
+import { IPost } from '../../interfaces/posts';
+
+import PostList from "../../components/PostList";
+import "./Home.css";
+
 
 export default function Home() {
   const { searchTerm } = useSearch();
-  const { data, isPending, error } = useFetch<IPost[]>("https://jsonplaceholder.typicode.com/posts");
+  const [start, setStart] = useState<number>(0);
+  const limit: number = 10;
 
-  const filteredPosts = data ? data.filter(post =>
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [loadingMore, setLoadingMore] = useState<Boolean>(false);
+
+  const url = `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`;
+  const { data, isPending, error } = useFetch<IPost[]>(url);
+
+  useEffect(() => {
+    if (data) {
+      setPosts((prevPosts) => [...prevPosts, ...data]);
+      setLoadingMore(false);
+    }
+  }, [data]);
+
+
+  const handleScroll = () => {
+    const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight;
+    if (bottom && !loadingMore) {
+      setStart((prevStart) => prevStart + limit);
+      setLoadingMore(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+  const filteredPosts = posts.filter(post =>
     post.title.toLowerCase().includes(searchTerm.toLowerCase())
-  ) : [];
-
+  );
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
-      {isPending && <p className="loading">Loading ...</p>}
-      {data && <PostList posts={filteredPosts}/>}
+      {isPending && posts.length === 0 && <p className="loading">Loading...</p>}
+      <PostList posts={filteredPosts} />
+      {loadingMore && <p>Loading more...</p>}
     </div>
   );
 }
